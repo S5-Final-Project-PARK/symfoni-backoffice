558ed4e650793bdd8b2c0398b4a1b5999be57315
Niors_haz <johanesa.arovaniavo@gmail.com>
2025-01-29 21:29:11 +0300

    try #36

--- public/index.php
+++ public/index.php
@@ -10,11 +10,13 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 error_log(print_r(getallheaders(), true));
 
 Request::setTrustedProxies(
-    [$_SERVER['REMOTE_ADDR']], // Proxy IPs to trust (adjust as needed)
+    [$_SERVER['REMOTE_ADDR']], // Could be set to proxy IP or '*'
     Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_HOST
 );
 
 Request::setTrustedHosts([getenv('APP_URL')]); // Replace with your actual app URL or domain
+
+error_log(print_r(getallheaders(), true));
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
