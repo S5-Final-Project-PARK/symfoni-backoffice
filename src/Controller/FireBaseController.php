724eb7f6793104f9d3dc38d5e38fe5073101022b
Niors_haz <johanesa.arovaniavo@gmail.com>
2025-02-07 08:54:14 +0300

    Update FireBaseController.php

--- src/Controller/FireBaseController.php
+++ src/Controller/FireBaseController.php
@@ -1,18 +1,21 @@
 <?php
-    use Kreait\Firebase\Factory;
-    use Kreait\Firebase\Auth;
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-    use Symfony\Component\HttpFoundation\JsonResponse;
-    use Symfony\Component\Routing\Annotation\Route;
 
-    class FireBaseController extends AbstractController
+namespace App\Controller;
+
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\Auth;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FirebaseController extends AbstractController
+{
+    #[Route('/firebase/test', name: 'firebase_test')]
+    public function testFirebase(): JsonResponse
     {
-        #[Route('/firebase/test', name: 'firebase_test')]
-        public function testFirebase(): JsonResponse
-        {
-            $firebase = (new Factory)->withServiceAccount(__DIR__ . '/../../config/firebase_credentials.json');
-            $auth = $firebase->createAuth();
+        $firebase = (new Factory)->withServiceAccount(__DIR__ . '/../../config/firebase_credentials.json');
+        $auth = $firebase->createAuth();
 
-            return new JsonResponse(['message' => 'Firebase connected successfully!']);
-        }
+        return new JsonResponse(['message' => 'Firebase connected successfully!']);
     }
+}
