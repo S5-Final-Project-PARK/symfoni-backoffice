352786950d38821ccafc1fb7f203d7d8451b4453
Niors_haz <johanesa.arovaniavo@gmail.com>
2025-02-07 08:48:14 +0300

    adding firebase

---
+++ src/Controller/FireBaseController.php
@@ -0,0 +1,18 @@
+<?php
+    use Kreait\Firebase\Factory;
+    use Kreait\Firebase\Auth;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\JsonResponse;
+    use Symfony\Component\Routing\Annotation\Route;
+
+    class FirebaseController extends AbstractController
+    {
+        #[Route('/firebase/test', name: 'firebase_test')]
+        public function testFirebase(): JsonResponse
+        {
+            $firebase = (new Factory)->withServiceAccount(__DIR__ . '/../../config/firebase_credentials.json');
+            $auth = $firebase->createAuth();
+
+            return new JsonResponse(['message' => 'Firebase connected successfully!']);
+        }
+    }
